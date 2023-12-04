67807e7f2db3f8a9fcde659ae2f1731aec1e6ef0
Tho Kien <74567462+kin280@users.noreply.github.com>
2023-12-04 20:07:59 +0700

    hello

---
+++ GU1-W02/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU1_W02
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("hello");
+        }
+    }
+}
